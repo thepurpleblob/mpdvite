8e084ea033e5f5350cbbe18d5fcb2a3bad3a6eec
Howard Miller <howardsmiller@gmail.com>
2025-06-10 07:32:39 +0100

    working on locos page

---
+++ src/components/FolderImage.vue
@@ -0,0 +1,35 @@
+<template>
+    <img class="img-fluid" :src="assets + '/' + image.filename_disk" style="height: 200px">
+</template>
+
+<script setup>
+    import {ref, onMounted, defineProps} from 'vue';
+    import axios from 'axios';
+
+    const image = ref({});
+    const assets = import.meta.env.VITE_APP_ENDPOINT + '/assets';
+
+    const props = defineProps({
+        folder: String,
+    });
+
+    onMounted(() => {
+        const endpoint = import.meta.env.VITE_APP_ENDPOINT;
+
+        const folderfilter = {
+            folder: {
+                _eq: props.folder,
+            }
+        };
+
+        axios.get(endpoint + '/files?limit=1&filter=' + JSON.stringify(folderfilter))
+        .then(result => {
+            window.console.log(result);
+            const data = result.data.data;
+            image.value = data[0];
+        })
+        .catch(error => {
+            window.console.error(error);
+        })
+    })
+</script>
--- src/views/LocosView.vue
+++ src/views/LocosView.vue
@@ -1,5 +1,47 @@
 <template>
-    <div class="my-4 alert alert-primary">
+    <div v-if="!locos" class="my-4 alert alert-primary">
         Nothing to see here, just yet. Please come back soon.
     </div>
+
+    <div v-if="locos">
+        <div v-for="loco in locos" class="card my-3" style="height: 200px">
+            <div class="row">
+                <div class="col-4">
+                    <FolderImage :folder="loco.folder"></FolderImage>
+                </div>
+                <div class="col-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ loco.title }}</h5>
+                        <p class="card-text" v-html="loco.content"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script setup>
+    import {ref, onMounted} from 'vue';
+    import axios from 'axios';
+    import FolderImage from '@/components/FolderImage.vue';
+
+    const locos = ref([]);
+
+    onMounted(() => {
+        const endpoint = import.meta.env.VITE_APP_ENDPOINT;
+
+        const published = {
+            status: {
+                _eq: 'published'
+            }
+        };
+        axios.get(endpoint + '/items/locos?filter' + JSON.stringify(published))
+        .then(result => {
+            const data = result.data.data;
+            locos.value = data;
+        })
+        .catch(error => {
+            window.console.error(error);
+        })
+    });
+</script>
